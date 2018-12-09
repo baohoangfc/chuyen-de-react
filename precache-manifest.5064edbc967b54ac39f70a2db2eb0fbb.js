39f24719a60fd0614c238471c480624e5dbc71c2
baohoangfc <baohoangfc@gmail.com>
2018-12-09 16:50:02 +0700

    Updates

---
+++ precache-manifest.5064edbc967b54ac39f70a2db2eb0fbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "745fb3fa919ec005d605",
+    "url": "/deploy-github/static/js/runtime~main.745fb3fa.js"
+  },
+  {
+    "revision": "8a13dce4165613669dc2",
+    "url": "/deploy-github/static/js/main.8a13dce4.chunk.js"
+  },
+  {
+    "revision": "5ee9e425f490731e7880",
+    "url": "/deploy-github/static/js/1.5ee9e425.chunk.js"
+  },
+  {
+    "revision": "8a13dce4165613669dc2",
+    "url": "/deploy-github/static/css/main.1ca8c793.chunk.css"
+  },
+  {
+    "revision": "a6424e2049269a0f1dac9c0996acc328",
+    "url": "/deploy-github/index.html"
+  }
+];
